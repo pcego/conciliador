ca1914a278d9179affcf28b86f8289d1815439aa
Gregory Pacheco <gpzim98@gmail.com>
2015-08-11 20:40:55 -0300

    Subindo o inicio das documentacoes e o requirements atualizado

---
+++ docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:46,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,hello:0,index:0,modul:0,page:0,search:0,world:0},titles:["Welcome to conciliador&#8217;s documentation!"],titleterms:{conciliador:0,document:0,indic:0,tabl:0,welcom:0}})
