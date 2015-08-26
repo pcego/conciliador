36db083f80c09127b4490d6786e87917062034d9
Gregory Pacheco <gpzim98@gmail.com>
2015-08-25 21:55:19 -0300

    404, 500, 403 e heranca de templates concluida

--- project/settings.py
+++ project/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'conciliador',
 )
 
 MIDDLEWARE_CLASSES = (
--- project/views.py
+++ project/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-	return HttpResponse('3F - Conciliador ... wait')
+	return render(request, 'index.html')
