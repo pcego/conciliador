8d7898e606d80c54bb63874961536468e8dddecc
pcego <pcego36@gmail.com>
2015-08-27 19:58:23 -0300

    implementing REQ003-004 and 005

--- conciliador/models.py
+++ conciliador/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class City(models.Model):
+	
+	name = models.CharField(max_length=100, blank=False)
+	state = models.ForeignKey(State)
+	active = models.BooleanField(default=True)
+	
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name = 'Cidade'
+
+class neighborhood(models.Model):
+		
+	name = models.CharField(max_length=100, blank=False)
+	city_id = models.ForeignKey(City)
+	active = models.BooleanField(default=True)	
+
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name = 'Bairro'
+
+class State(models.Model):
+		
+	name = models.CharField(max_length=100, blank=False)
+	tag = models.CharField(max_length=2, blank=False)	
+	active = models.BooleanField(default=True)
+	
+	def __str__(self):
+		return self.name
+
+	class Meta:
+		verbose_name = 'Estado'
